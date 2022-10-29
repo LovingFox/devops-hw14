d484e812717d40a1f4bb6e890d360cddc51facd8
Ivan Revyakin <LovingFox@GMail.com>
2022-10-29 10:14:35 +0300

    .

--- main.tf
+++ main.tf
@@ -10,4 +10,23 @@ resource "local_file" "ssh_key-keyfile" {
   content = tls_private_key.ssh_key.private_key_openssh
 }
 
+resource "aws_security_group" "sg_ssh" {
+  name = var.securityGroup
+  description = "[Terraform] Allow SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # end of main.tf
