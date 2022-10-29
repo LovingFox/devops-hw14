fadbcf56d1b30245c30c68ce3838e4d68f99db16
Ivan Revyakin <LovingFox@GMail.com>
2022-10-29 10:01:27 +0300

    init

---
+++ main.tf
@@ -0,0 +1,13 @@
+# main.tf
+
+resource "tls_private_key" var.keyName {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "${var.keyName}-keyfile" {
+  filename = var.keyFile
+  content = "${tls_private_key.${var.keyName}.private_key_openssh}"
+}
+
+# end of main.tf
---
+++ variables.tf
@@ -0,0 +1,44 @@
+# variables.tf
+
+variable "access_key" {
+   default = "<PUT IN YOUR AWS ACCESS KEY or use 'export TF_VAR_access_key='>"
+}
+
+variable "secret_key" {
+   default = "<PUT IN YOUR AWS SECRET KEY or use 'export TF_VAR_secret_key='>"
+}
+
+variable "region" {
+   default = "eu-central-1"
+}
+
+variable "availabilityZone" {
+   default = "eu-central-1a"
+}
+
+variable "instanceType" {
+   default = "t2.micro"
+}
+
+variable "instanceName" {
+   default = "devops-hw14-builder"
+}
+
+variable "keyName" {
+   default = "${var.instanceName}-key"
+}
+
+variable "keyFile" {
+   default = "./${var.instanceName}.key"
+}
+
+variable "securityGroup" {
+   default = "sg-${var.InstanceName}"
+}
+
+# Ubuntu 22.04
+variable "ami" {
+   default = "ami-0caef02b518350c8b"
+}
+
+# end of variables.tf
