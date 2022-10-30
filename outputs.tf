578d9f79dd25ffdda681acb10962721b27a71da3
Ivan Revyakin <LovingFox@GMail.com>
2022-10-30 13:41:45 +0300

    output update

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,18 @@
 # outputs.tf
 
-output "builder_dns_name" {
-  value = aws_instance.builder_instance.public_dns
-  description = "DNS name of the host"
-}
-
 output "bucket" {
   value = aws_s3_bucket.s3_bucket.bucket
   description = "bucket"
+}
+
+output "builder_dns_name" {
+  value = aws_instance.builder_instance.public_dns
+  description = "DNS name of the Builder"
+}
+
+output "webserver_dns_name" {
+  value = aws_instance.webserver_instance.public_dns
+  description = "DNS name of the Webserver"
 }
 
 # end of outputs.tf
